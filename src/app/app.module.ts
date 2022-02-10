c604ae8a3189a030848512597583c99d8fdf1c3b
Mercy <mercycherotich757@gmail.com>
2022-02-10 14:45:02 +0300

    create pipe for date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { FormComponent } from './form/form.component';
+import { DatePipe } from './date.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    FormComponent
+    FormComponent,
+    DatePipe
   ],
   imports: [
     BrowserModule,
