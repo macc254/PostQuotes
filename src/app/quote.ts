8358e22ee8effc85177f4c65bf31751ffb7eaf98
Mercy <mercycherotich757@gmail.com>
2022-02-10 14:53:31 +0300

    define the values inside the class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,18 @@
 export class Quote {
-}
+    showAuthor: boolean;
+    id: number | undefined;
+      constructor(
+        public name: string, 
+        public author: string, 
+        public person: string, 
+        public quoteDate: Date, 
+        public upvote: number = 0, 
+        public downvote: number = 0, 
+        public isFavorite: boolean
+        )
+      {     
+           this.showAuthor=false;
+      }
+  
+  }
+  
