3e7d5a4d54fbde232dbd1a1545a63c4faaf66100
Mercy <mercycherotich757@gmail.com>
2022-02-10 15:13:22 +0300

    install bootstrap extension

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit,  Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailComponent implements OnInit {
 
+  @Input() quote!:Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+quoteDelete(complete:boolean){
+  this.isComplete.emit(complete);
+}
+public addvote(quote1:any){
+  quote1.upvote++;
+}
+
+public reducevote(quote1:any){
+  quote1.downvote++;
+}
   constructor() { }
 
   ngOnInit(): void {
   }
+  quotes: Quote[] = [
+    ];
 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
-  constructor() { }
-
+  @Input() quote!:Quote;
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+  quotes: Quote[] = [
+   new Quote('The greatest glory in living lies not in never falling, but in rising every time we fall. ','Nelson Mandela','Mercy Bore',new Date(2020,3,13),0,0,false),
+   new Quote('The way to get started is to quit talking and begin doing.','Walt Disney','Mercy Bore',new Date(2020,3,13),0,0,false)
+   ];
 
+  toggleAuthor(index: any){
+    this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
+  }
+  deleteQuote(isComplete: any, index: number){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+
+      if (toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
+  }
+  addNewQuote(quote: Quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.quoteDate = new Date(quote.quoteDate);
+    this.quotes.push(quote);
+  }
+  
 }
